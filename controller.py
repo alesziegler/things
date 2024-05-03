ae50180b4fd3a07b76eb7c846ed6c5616a9c4b3e
alesziegler <alesziegler@gmail.com>
2024-05-03 20:13:44 +0200

    First draft of the basic structure

---
+++ controller.py
@@ -0,0 +1,36 @@
+
+from repeater import Repeater
+class Controller:
+
+    def __init__(self,chosen_action):
+        self.chosen_action = chosen_action
+        if self.chosen_action == 4:
+            self.ending()
+        else:
+            if self.chosen_action == 1:
+                self.add_new_customer()
+            elif self.chosen_action == 2:
+                self.print_all_customers()
+            elif self.chosen_action == 3:
+                self.find_customer()
+            else:
+                self.error_messaging()
+            Repeater("Pokracujte libovolnou klavesou...")
+
+    def add_new_customer(self):
+        pass
+
+    def print_all_customers(self):
+        pass
+
+    def find_customer(self):
+        pass
+
+    def ending(self):
+        print('This is The End')
+
+    def error_messaging(self):
+        self.chosen_action = int(
+            input("REPENT:")
+        )
+        return self
---
+++ repeater.py
@@ -0,0 +1,12 @@
+
+class Repeater:
+    """
+    Only purpose of an object of this class is to ensure that
+    new controller object is initialized every time
+    previous interaction ends, until users ends the program.
+    (I don't know whether this is a 'proper' way to do it):
+    """
+    def __init__(self,message_to_user):
+        print(message_to_user)
+        
+
