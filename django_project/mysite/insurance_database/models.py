a9baac48293d1f0f706b0b6aa580e53beeab27f1
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-05-24 14:34:26 +0000

    Starting with Django

--- django_project/mysite/insurance_database/models.py
+++ django_project/mysite/insurance_database/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+  name = models.CharField(max_length=200)
+  age = models.IntegerField(max_length=5)
+  contact = models.CharField(lenght=9)
--- django_project/mysite/insurance_database/views.py
+++ django_project/mysite/insurance_database/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def customer_detail(request):
+  return render(
+    request, "insurance_database/customer_detail.html",
+    dict(name="Josef Novak", age=30, contact="222111000")
+  )
