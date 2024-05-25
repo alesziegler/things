2a6c6fb4ecd0b1f65e3e50424c64a3279bb99165
alesziegler <alesziegler@gmail.com>
2024-05-25 17:45:47 +0200

    Saving bootstrap attempt

--- django_project/mysite/insurance_database/urls.py
+++ django_project/mysite/insurance_database/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("main_table/", views.MainTable.as_view(), name="customer_index"),
+path("main_table/", views.MainTable.as_view(), name="main_table"),
 path("add_customer/", views.AddCustomer.as_view(), name="add_customer"),
 ]
--- django_project/mysite/mysite/settings.py
+++ django_project/mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'insurance_database',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
